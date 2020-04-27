e207ccc71bcd6c300a5b2b01f8eb766b5a2d2640
Ajaysonker <ajaysonker14@gmail.com>
2020-04-27 17:46:36 +0530

    My_Post

---
+++ Posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+
+# Create your models here.
+class PostCot(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+# Create your models here.
+class Posts(models.Model):
+    title = models.ForeignKey(PostCot, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now=True)
+    update_date = models.DateTimeField(auto_now_add=True)
+    sort_info = models.TextField()
+    desc = RichTextField()
+
+    def __str__(self):
+        return self.name
